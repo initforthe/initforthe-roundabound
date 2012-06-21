fce599e738a1058bfe3b4162cd79fcd6e2c64964
Tom Simnett <tom@initforthe.com>
2012-06-21 15:00:09 +0100

    Added lib to set up railtie stuffins, moving assets to vendor

---
+++ lib/initforthe-roundabound.rb
@@ -0,0 +1,1 @@
+require 'initforthe-roundabound/engine'
---
+++ lib/initforthe-roundabound/engine.rb
@@ -0,0 +1,7 @@
+module Initforthe
+  module Roundabound
+    class Engine < ::Rails::Engine
+      # Ohai, Rails, I has some vendor for you!
+    end
+  end
+end
